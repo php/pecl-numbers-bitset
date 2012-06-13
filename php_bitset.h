b8864809e678439e5401733ea15f5c0a7bb31b04
Felipe Pena <felipe@php.net>
2012-06-13 12:16:36 +0000

    - Preparing release

--- php_bitset.h
+++ php_bitset.h
@@ -24,7 +24,7 @@
 extern zend_module_entry bitset_module_entry;
 #define phpext_bitset_ptr &bitset_module_entry
 
-#define PHP_BITSET_VERSION "1.0.1-dev"
+#define PHP_BITSET_VERSION "1.0.1"
 
 
 #ifdef ZTS
